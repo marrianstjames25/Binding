47ea522e9300d8749c49c320eee3c61e21d7db86
marrianstjames25 <72973155+marrianstjames25@users.noreply.github.com>
2023-01-31 12:05:10 +0200

    Add project files.

---
+++ Binding/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Windows;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Binding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+
+        private int _boundNumber;
+        public int BoundNumber
+        {
+            get { return _boundNumber; }
+
+            set
+            {
+                if(_boundNumber != value)
+                {
+                    _boundNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BoundNumber = 0;
+        }
+    }
+}
